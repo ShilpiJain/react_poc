0e1b4ef319fd9b3ae631c6f9833781c97ca0ec63
Shilpi Jain <>
2019-06-04 15:21:27 +0530

    form structure and building

--- src/client/components/login/login.jsx
+++ src/client/components/login/login.jsx
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
-import '../Login/styles/style.scss';
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-
+import './styles/login.scss';
+import LoginForm from './container/LoginForm';
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loginDetails : {
+                phone : '',
+                password :''
+            },
+            values : {
+                showPassword : false,
+                username : '',
+                password :''
+            }
+        }
+        this.onFocusHandle = this.onFocusHandle.bind(this);
+        this.onBlurHandle = this.onBlurHandle.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onsubmit = this.onsubmit.bind(this);
+        
+    }
+    onFocusHandle() {
+
+    }
+    onBlurHandle() {
+
+    }
+    onInputChange() {
+
+    }
+    onsubmit() {
+        
+    }
     render() {
         return (
             <div>
                 {/* <Button>Submit</Button>
                 <Input /> */}
-                <form>
-                    <Input/>
-                </form>
+                <LoginForm 
+                    values={this.state.values}
+                />
             </div>
         )
     }
---
+++ src/client/constants/index.js
@@ -0,0 +1,4 @@
+export default {
+    USER_NAME : 'Username',
+    PASSWORD : 'Password'
+}
